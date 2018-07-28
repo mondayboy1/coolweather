dd5e8c607ed0bc2d550b7a09ce6a37603b8a01eb
mondayboy <1091060801@qq.com>
2018-07-28 11:26:13 +0800

    后台自动更新和显示天气的功能

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.coolweather.android;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -78,7 +80,25 @@ public class ChooseAreaFragment extends Fragment{
                 else if (currentLevel==LEVEL_CITY)
                 {
                     selectedCity=cityList.get(position);
-                    queryCities();
+                    queryCounties();
+                }
+                else if (currentLevel==LEVEL_COUNTY)
+                {
+                    String weatherId=countyList.get(position).getWeatherId();
+                    if (getActivity()instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+                    else if (getActivity() instanceof WeatherActivity)
+                    {
+                        WeatherActivity activity=(WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+
+                    }
                 }
             }
         });
@@ -95,6 +115,7 @@ public class ChooseAreaFragment extends Fragment{
                 {
                     queryProvinces();
                 }
+
             }
         });
         queryProvinces();
---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by includeiostream on 2018/7/26.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity
+    {
+        public String aqi;
+        public String pm25;
+    }
+}
+
